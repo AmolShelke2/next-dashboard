c1e6f078a3bd732c1ceb1336f47d1eedac0dddd0
Amol <shelkeamol897@gmail.com>
2023-08-03 21:58:34 +0530

    [feat]: sidebar, header and cards created

---
+++ components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <div className="flex justify-between px-4 pt-4 mb-2 border-b-[1px]">
+      <h2 className="text-sm lg:text-xl whitespace-nowrap capitalize mb-2">
+        dashboard
+      </h2>
+      <h2 className="text-sm lg:text-xl whitespace-nowrap capitalize mb-2">
+        Welcom back, John
+      </h2>
+    </div>
+  );
+};
